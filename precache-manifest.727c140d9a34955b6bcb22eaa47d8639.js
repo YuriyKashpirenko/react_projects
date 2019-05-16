71df7ee808176bb81e46e930aa004a87b273e62e
YuriyKashpirenko <36638828+YuriyKashpirenko@users.noreply.github.com>
2019-05-15 21:32:25 -0400

    Update precache-manifest.js
    Edit url
    from "url": "/YuriyKashpirenko/react_projects.git/
    to "url": "/YuriyKashpirenko/react_projects/

--- precache-manifest.727c140d9a34955b6bcb22eaa47d8639.js
+++ precache-manifest.727c140d9a34955b6bcb22eaa47d8639.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "c3c68b40cc8bc83398e7c2dae068cf36",
-    "url": "/YuriyKashpirenko/react_projects.git/index.html"
+    "url": "/YuriyKashpirenko/react_projects/index.html"
   },
   {
     "revision": "f03359400e980bf62f85",
-    "url": "/YuriyKashpirenko/react_projects.git/static/css/main.129ebed6.chunk.css"
+    "url": "/YuriyKashpirenko/react_projects/static/css/main.129ebed6.chunk.css"
   },
   {
     "revision": "ec666c2ebdc1b1a9c926",
-    "url": "/YuriyKashpirenko/react_projects.git/static/js/2.0fe81a13.chunk.js"
+    "url": "/YuriyKashpirenko/react_projects/static/js/2.0fe81a13.chunk.js"
   },
   {
     "revision": "f03359400e980bf62f85",
-    "url": "/YuriyKashpirenko/react_projects.git/static/js/main.04a53d50.chunk.js"
+    "url": "/YuriyKashpirenko/react_projects/static/js/main.04a53d50.chunk.js"
   },
   {
     "revision": "108ed690a355c9b10728",
-    "url": "/YuriyKashpirenko/react_projects.git/static/js/runtime~main.b3851811.js"
+    "url": "/YuriyKashpirenko/react_projects/static/js/runtime~main.b3851811.js"
   }
 ]);
